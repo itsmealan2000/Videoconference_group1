d62248350d3871e7a4753a991005acb06149c454
Sri Janani <srijananisriju@gmail.com>
2024-09-18 00:15:17 +0530

    Added voice call and viedo call icon for each of the contacts

--- lib/contacts_page.dart
+++ lib/contacts_page.dart
@@ -1,82 +1,96 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:contacts_service/contacts_service.dart';
-import 'package:videoconference/ChatPage.dart';
 
 class ContactsPage extends StatelessWidget {
   final List<Contact> contacts;
+  final Function(Contact) onContactSelected;
 
-  const ContactsPage({super.key, required this.contacts});
+  const ContactsPage({
+    super.key,
+    required this.contacts,
+    required this.onContactSelected,
+  });
 
   @override
   Widget build(BuildContext context) {
-    final List<Contact> validContacts = contacts.where((contact) {
-      final String displayName = contact.displayName ?? '';
-      final String phoneNumber = contact.phones?.isNotEmpty == true
-          ? contact.phones!.first.value ?? ''
-          : '';
-      return displayName.isNotEmpty || phoneNumber.isNotEmpty;
-    }).toList();
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Contacts'),
       ),
-      body: validContacts.isEmpty
-          ? const Center(child: Text('No valid contacts found'))
-          : ListView.separated(
-              itemCount: validContacts.length,
-              separatorBuilder: (context, index) => const Divider(),
-              itemBuilder: (context, index) {
-                final contact = validContacts[index];
-                final String displayName = contact.displayName ?? 'No Name';
-                final String phoneNumber = contact.phones?.isNotEmpty == true
-                    ? contact.phones!.first.value ?? 'No Number'
-                    : 'No Number';
+      body: ListView.builder(
+        itemCount: contacts.length,
+        itemBuilder: (context, index) {
+          final contact = contacts[index];
+          final displayName = contact.displayName ?? 'No Name';
+          final phoneNumber = contact.phones?.isNotEmpty == true
+              ? contact.phones!.first.value ?? 'No Number'
+              : 'No Number';
 
-                return ListTile(
-                  leading: CircleAvatar(
-                    radius: 25,
-                    backgroundColor: Colors.blueAccent,
-                    backgroundImage:
-                        contact.avatar != null && contact.avatar!.isNotEmpty
-                            ? MemoryImage(contact.avatar!)
-                            : null,
-                    child: contact.avatar == null || contact.avatar!.isEmpty
-                        ? Text(
-                            displayName.isNotEmpty
-                                ? displayName[0].toUpperCase()
-                                : phoneNumber[0],
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                            ),
-                          )
-                        : null,
-                  ),
-                  title: Text(
-                    displayName.isNotEmpty ? displayName : phoneNumber,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: Text(
-                    phoneNumber,
-                    style: const TextStyle(color: Colors.grey),
-                  ),
-                  onTap: () {
-                    // Navigate to the chat page when a contact is clicked
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ChatPage(
-                          contactName:
-                              displayName.isNotEmpty ? displayName : '',
-                          phoneNumber: phoneNumber,
-                        ),
-                      ),
-                    );
-                  },
-                );
-              },
-            ),
+          // Skip contacts without name or number
+          if (displayName == 'No Name' && phoneNumber == 'No Number') {
+            return const SizedBox.shrink();
+          }
+
+          return Column(
+            children: [
+              ListTile(
+                leading: CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Colors.blueAccent,
+                  backgroundImage:
+                      contact.avatar != null && contact.avatar!.isNotEmpty
+                          ? MemoryImage(contact.avatar!)
+                          : null,
+                  child: contact.avatar == null || contact.avatar!.isEmpty
+                      ? Text(
+                          displayName.isNotEmpty
+                              ? displayName[0].toUpperCase()
+                              : phoneNumber[0],
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        )
+                      : null,
+                ),
+                title: Text(
+                  displayName.isNotEmpty ? displayName : phoneNumber,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(phoneNumber),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.call, color: Colors.green),
+                      onPressed: () {
+                        // Trigger voice call functionality (implement accordingly)
+                        if (kDebugMode) {
+                          print('Voice call to $phoneNumber');
+                        }
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.videocam, color: Colors.blue),
+                      onPressed: () {
+                        // Trigger video call functionality (implement accordingly)
+                        if (kDebugMode) {
+                          print('Video call to $phoneNumber');
+                        }
+                      },
+                    ),
+                  ],
+                ),
+                onTap: () {
+                  onContactSelected(contact);
+                },
+              ),
+              const Divider(), // Line between contacts
+            ],
+          );
+        },
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:videoconference/auth/auth_page.dart';
-import 'package:videoconference/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:contacts_service/contacts_service.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:videoconference/ChatPage.dart';
+import 'firebase_options.dart';
 import 'contacts_page.dart';
 
 void main() async {
@@ -12,128 +12,185 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<Contact> _recentChats = []; // List to keep track of recent chats
+
+  void _addRecentChat(Contact contact) {
+    setState(() {
+      if (!_recentChats.contains(contact)) {
+        _recentChats.add(contact);
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Video Conference App',
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: HomePage(
+        recentChats: _recentChats,
+        onChatStarted: _addRecentChat,
+      ),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomePage extends StatelessWidget {
+  final List<Contact> recentChats;
+  final Function(Contact) onChatStarted;
 
-  @override
-  // ignore: library_private_types_in_public_api
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  String _headerTitle = 'Auth Page'; // Initial header title
-
-  // Function to update header title
-  void _updateHeaderTitle(String newTitle) {
-    setState(() {
-      _headerTitle = newTitle;
-    });
-  }
-
-  // Fetch contacts and navigate to the ContactsPage
-  Future<void> _fetchContactsAndNavigate() async {
-    // Request permission to access contacts
-    PermissionStatus permissionStatus = await Permission.contacts.request();
-
-    if (permissionStatus.isGranted) {
-      // Fetch contacts
-      List<Contact> contacts = await ContactsService.getContacts();
-      // Navigate to the ContactsPage
-      if (mounted) {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => ContactsPage(contacts: contacts),
-          ),
-        );
-      }
-    } else {
-      // Handle permission denied
-      // ignore: use_build_context_synchronously
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Permission to access contacts denied')),
-      );
-    }
-  }
+  const HomePage(
+      {super.key, required this.recentChats, required this.onChatStarted});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_headerTitle), // Dynamic header title
-        centerTitle: true,
+        title: const Text('Home Page'),
       ),
-      body: Stack(
+      body: Column(
         children: [
-          const AuthPage(), // Replace with your page content
-          Positioned(
-            bottom: 20, // Position above the footer
-            right: 20, // Right corner
-            child: FloatingActionButton(
-              onPressed: () async {
-                await _fetchContactsAndNavigate(); // Fetch contacts and navigate to ContactsPage
-                _updateHeaderTitle('Contacts');
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Recent Chats',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: recentChats.length,
+              itemBuilder: (context, index) {
+                final contact = recentChats[index];
+                final displayName = contact.displayName ?? 'No Name';
+                final phoneNumber = contact.phones?.isNotEmpty == true
+                    ? contact.phones!.first.value ?? 'No Number'
+                    : 'No Number';
+                return ListTile(
+                  leading: CircleAvatar(
+                    radius: 25,
+                    backgroundColor: Colors.blueAccent,
+                    backgroundImage:
+                        contact.avatar != null && contact.avatar!.isNotEmpty
+                            ? MemoryImage(contact.avatar!)
+                            : null,
+                    child: contact.avatar == null || contact.avatar!.isEmpty
+                        ? Text(
+                            displayName.isNotEmpty
+                                ? displayName[0].toUpperCase()
+                                : phoneNumber[0],
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                          )
+                        : null,
+                  ),
+                  title: Text(
+                    displayName.isNotEmpty ? displayName : phoneNumber,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    phoneNumber,
+                    style: const TextStyle(color: Colors.grey),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ChatPage(
+                          contactName:
+                              displayName.isNotEmpty ? displayName : '',
+                          phoneNumber: phoneNumber,
+                        ),
+                      ),
+                    ).then((_) {
+                      onChatStarted(
+                          contact); // Add contact to recent chats after returning from chat page
+                    });
+                  },
+                );
               },
-              tooltip: 'Contact',
-              child: const Icon(Icons.contacts),
             ),
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.contacts),
+        onPressed: () async {
+          if (await Permission.contacts.request().isGranted) {
+            List<Contact> contacts = await ContactsService.getContacts();
+            Navigator.push(
+              // ignore: use_build_context_synchronously
+              context,
+              MaterialPageRoute(
+                builder: (context) => ContactsPage(
+                  contacts: contacts,
+                  onContactSelected: (contact) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ChatPage(
+                          contactName: contact.displayName ?? 'No Name',
+                          phoneNumber:
+                              contact.phones?.first.value ?? 'No Number',
+                        ),
+                      ),
+                    ).then((_) {
+                      onChatStarted(
+                          contact); // Add contact to recent chats after returning from chat page
+                    });
+                  },
+                ),
+              ),
+            );
+          } else {
+            // Handle permission denied case
+            // ignore: use_build_context_synchronously
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Contacts permission denied')),
+            );
+          }
+        },
+      ),
       bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              IconButton(
-                icon: const Icon(Icons.chat),
-                onPressed: () {
-                  _updateHeaderTitle('Chat');
-                  // Add chat functionality here
-                },
-                tooltip: 'Chat',
-              ),
-              IconButton(
-                icon: const Icon(Icons.call),
-                onPressed: () {
-                  _updateHeaderTitle('Voice Call');
-                  // Add voice call functionality here
-                },
-                tooltip: 'Voice Call',
-              ),
-              IconButton(
-                icon: const Icon(Icons.videocam),
-                onPressed: () {
-                  _updateHeaderTitle('Video Call');
-                  // Add video call functionality here
-                },
-                tooltip: 'Video Call',
-              ),
-              IconButton(
-                icon: const Icon(Icons.settings),
-                onPressed: () {
-                  _updateHeaderTitle('Settings');
-                  // Add settings functionality here
-                },
-                tooltip: 'Settings',
-              ),
-            ],
-          ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.chat),
+              onPressed: () {
+                // Chat button functionality
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.call),
+              onPressed: () {
+                // Voice call button functionality
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.videocam),
+              onPressed: () {
+                // Video call button functionality
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () {
+                // Settings button functionality
+              },
+            ),
+          ],
         ),
       ),
     );
