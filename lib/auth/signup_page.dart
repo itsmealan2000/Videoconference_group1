8054f3772106ffad8733e32b499926ca9f6bf812
Srijanani B <srijananisriju@gmail.com>
2024-10-28 22:32:38 +0530

    Update signup_page.dart for resolving confilt

--- lib/auth/signup_page.dart
+++ lib/auth/signup_page.dart
@@ -3,7 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-// import 'package:videoconference/auth/complete.dart';
+//import 'package:videoconference/auth/complete.dart';
  import 'package:videoconference/auth/complete_profile.dart';
 //import 'package:videoconference/pages/home_page.dart';
 
@@ -255,5 +255,4 @@ class _SignupPageState extends State<SignupPage> {
     );
   }
 }
-
 
